6f76e6d726d4a73d10ec2faca7b00b8b32ed111b
Ana Maria Horhoianu <ana_horhoianu@yahoo.com>
2021-06-20 17:36:55 +0100

    game

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("_includes");
+    eleventyConfig.addPassthroughCopy("layouts");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
