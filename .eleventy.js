d8a5d43a36481ea53eaa86e9ff353f375d8601ab
Ana Maria Horhoianu <ana_horhoianu@yahoo.com>
2021-06-20 17:58:59 +0100

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,23 @@
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("_includes");
-    eleventyConfig.addPassthroughCopy("layouts");
-    eleventyConfig.addPassthroughCopy("admin");
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('images')
+    eleventyConfig.addPassthroughCopy('admin')
     eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("js");
-    eleventyConfig.addPassthroughCopy("source");
-        data: "_data"
-};
+    const {
+      DateTime
+    } = require("luxon");
+  
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+      eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {
+          zone: 'utc'
+        }).toFormat('yy-MM-dd');
+      });
+  
+      eleventyConfig.addFilter("readableDate", dateObj => {
+      return DateTime.fromJSDate(dateObj, {
+        zone: 'utc'
+      }).toFormat("dd-MM-yy");
+    });
+  
+  };
+  
